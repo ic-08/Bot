b1c98f34e5b6d7ff6cd7b6c5d068b74ad5b1d100
TheJokingJack <jokingjames321@gmail.com>
2021-08-26 14:57:43 -0400

    allowed to reply to message with 'hi', 'hello'

--- src/Bot.py
+++ src/Bot.py
@@ -78,7 +78,10 @@ async def on_message(message):
                 pending_msg = search(command[0])
         try:
             if pending_msg['msg_type'] == 'txt':
-                await message.channel.send(pending_msg['building_msg'])
+                if pending_msg['msg_args']['reply'] == True:
+                    await message.reply(pending_msg['building_msg'])
+                else:
+                    await message.channel.send(pending_msg['building_msg'])
             elif pending_msg['msg_type'] == 'embed':
                 await message.channel.send(embed = pending_msg['building_msg'])
             elif pending_msg['msg_type'] == 'timer':
