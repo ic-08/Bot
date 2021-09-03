c3c03fd71e5ff6240559594a02142068e8b3f682
ic-08 <89464943+ic-08@users.noreply.github.com>
2021-09-03 14:04:54 -0400

    updated again

--- scheduler.py
+++ scheduler.py
@@ -1,87 +1,110 @@
-#If you touch this code you will die with James's flying axe. 
-#James pls let me borrow it unless its you... 
+import threading
+from threading import Thread
+from datetime import *
+import discord
+import random
+from discord.ext import commands
+
+client = discord.Client()
+
 def scheduler(): 
     import os
     from PIL import Image, ImageDraw, ImageFont
     import pytz
+    import time
+    #testing
+    while True:
+        if str(datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M")) == str(datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M")):
+            print("hello time is now 11:00")
+            break
+        else:
+            print(str(datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M")))
+            time.sleep(10)
+
     #Open the background
     pics = os.listdir('assets/backgrounds')
     picture = random.choice(pics)
     im = Image.open(f'assets/backgrounds/{picture}')
 
-        #if blah:
-            ######## Day of the week ########
-            time_list = str(datetime.now(pytz.timezone('US/Eastern')))
-            print(time_list)
-            time_list = list(time_list)
-            ftime = ''
-            print(time_list)
-            x = datetime.now(pytz.timezone('US/Eastern')).weekday()
-            day_of_the_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-            print(day_of_the_week[x])
 
-            # Edit the picture for day of week
-            d = ImageDraw.Draw(im) 
-            fnt1 = ImageFont.truetype(f'assets/arial.ttf', 40)
-            width = fnt1.getsize(day_of_the_week[x])[0]
-            d.text((315 - round(width/2),400), day_of_the_week[x], fill=(255, 255, 255), font=fnt1) 
+    ######## Day of the week ########
+    time_list = str(datetime.now(pytz.timezone('US/Eastern')))
+    print(time_list)
+    time_list = list(time_list)
+    ftime = ''
+    print(time_list)
+    x = datetime.now(pytz.timezone('US/Eastern')).weekday()
+    day_of_the_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    print(day_of_the_week[x])
 
-            #Line
-            second_image = Image.open("assets/backgrounds/line.png") 
-            newsize = (width, 5)
-            second_image = second_image.resize(newsize)
-            im.paste(second_image, (315 - round(width/2),450))
+    # Edit the picture for day of week
+    d = ImageDraw.Draw(im) 
+    fnt1 = ImageFont.truetype(f'assets/arial.ttf', 40)
+    width = fnt1.getsize(day_of_the_week[x])[0]
+    d.text((315 - round(width/2),400), day_of_the_week[x], fill=(255, 255, 255), font=fnt1) 
 
-
-
-            ######## MONTH AND DATE ########
-            # Order = August 26th, 2021
-
-            #Find the month
-            fnt2 = ImageFont.truetype(f'assets/arial.ttf', 30)
-            months = ['January ', 'Feburary ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ', 'September ', 'October ', 'November ', 'December ']
-            dt = datetime.now(pytz.timezone('US/Eastern'))
-            full_date = str(months[dt.month -1]) + str(dt.day) + ','
-            width = fnt2.getsize(full_date)[0]
-            print(width)
-
-            #Edit the picture for the corresponding month
-            fnt2 = ImageFont.truetype(f'assets/arial.ttf', 35)
-            d.text((315 - round(width/2),580), full_date, fill=(255, 255, 255), font=fnt2) 
-            d.text((285,625), str(dt.year), fill=(255, 255, 255), font=fnt2, stroke_width=1, stroke_fill="white") 
+    #Line
+    second_image = Image.open("assets/backgrounds/line.png") 
+    newsize = (width, 5)
+    second_image = second_image.resize(newsize)
+    im.paste(second_image, (315 - round(width/2),450))
 
 
-            ######## TIME ########
-            for i in range(5):
-                ftime += str(time_list[i+11])
-            ftime = list(ftime)
-            fnt = ImageFont.truetype(f'assets/arial.ttf', 82)
 
-            # Positioning of Time Text
-            width, height = im.size 
-            wtime = []
-            for i in range(5):
-                width = fnt.getsize(ftime[i])[0]
-                wtime.append(width)
-            print(wtime)
-            x = 305
-            y = 475
+    ######## MONTH AND DATE ########
+    # Order = August 26th, 2021
 
-            #Distance between characters is exactly 26 pixels
-            d.text((round(x-(wtime[1]/2 + wtime[2]/2+26)) - round(wtime[0]/2 + wtime[1]/2 + 26),y), ftime[0], fill=(255, 255, 255), font=fnt, stroke_width=2,
-                stroke_fill="white") 
-            d.text((round(x-(wtime[1]/2 + wtime[2]/2+26)),y), ftime[1], fill=(255, 255, 255), font=fnt, stroke_width=2,
-                stroke_fill="white") 
-            d.text((x,y), ftime[2], fill=(255, 255, 255), font=fnt, stroke_width=2,
-                stroke_fill="white") 
-            d.text((round(x+(wtime[3]/2 + wtime[2]/2+5)),y), ftime[3], fill=(255, 255, 255), font=fnt, stroke_width=2,
-                stroke_fill="white") 
-            d.text((round(x+(wtime[3]/2 + wtime[2]/2+5)) + round(wtime[3]/2 + wtime[4]/2 + 26),y), ftime[4], fill=(255, 255, 255), font=fnt, stroke_width=2,
-                stroke_fill="white") 
+    #Find the month
+    fnt2 = ImageFont.truetype(f'assets/arial.ttf', 30)
+    months = ['January ', 'Feburary ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ', 'September ', 'October ', 'November ', 'December ']
+    dt = datetime.now(pytz.timezone('US/Eastern'))
+    full_date = str(months[dt.month -1]) + str(dt.day) + ','
+    width = fnt2.getsize(full_date)[0]
+    print(width)
+
+    #Edit the picture for the corresponding month
+    fnt2 = ImageFont.truetype(f'assets/arial.ttf', 35)
+    d.text((315 - round(width/2),580), full_date, fill=(255, 255, 255), font=fnt2) 
+    d.text((285,625), str(dt.year), fill=(255, 255, 255), font=fnt2, stroke_width=1, stroke_fill="white") 
+    
+    
+    ######## TIME ########
+    for i in range(5):
+        ftime += str(time_list[i+11])
+    ftime = list(ftime)
+    fnt = ImageFont.truetype(f'assets/arial.ttf', 82)
+
+    # Positioning of Time Text
+    width, height = im.size 
+    wtime = []
+    for i in range(5):
+        width = fnt.getsize(ftime[i])[0]
+        wtime.append(width)
+    print(wtime)
+    x = 305
+    y = 475
+
+    #Distance between characters is exactly 26 pixels
+    d.text((round(x-(wtime[1]/2 + wtime[2]/2+26)) - round(wtime[0]/2 + wtime[1]/2 + 26),y), ftime[0], fill=(255, 255, 255), font=fnt, stroke_width=2,
+        stroke_fill="white") 
+    d.text((round(x-(wtime[1]/2 + wtime[2]/2+26)),y), ftime[1], fill=(255, 255, 255), font=fnt, stroke_width=2,
+        stroke_fill="white") 
+    d.text((x,y), ftime[2], fill=(255, 255, 255), font=fnt, stroke_width=2,
+        stroke_fill="white") 
+    d.text((round(x+(wtime[3]/2 + wtime[2]/2+5)),y), ftime[3], fill=(255, 255, 255), font=fnt, stroke_width=2,
+        stroke_fill="white") 
+    d.text((round(x+(wtime[3]/2 + wtime[2]/2+5)) + round(wtime[3]/2 + wtime[4]/2 + 26),y), ftime[4], fill=(255, 255, 255), font=fnt, stroke_width=2,
+        stroke_fill="white") 
 
 
-            #Save the edited image
-            im.save(f'assets/backgrounds/temp.png')
-            await message.channel.send(file=discord.File(f'assets/backgrounds/temp.png'))
-            import os
-            os.remove(f'assets/backgrounds/temp.png')
+    #Save the edited image
+    im.save(f'assets/backgrounds/temp.png')
+    #channel = client.get_channel(842823949037076520) 
+    #await message.channel.send('hello') 
+    channel.send(file=discord.File(f'assets/backgrounds/temp.png'))
+    import os
+    os.remove(f'assets/backgrounds/temp.png')
+
+def scheduler_run():
+    t1 = Thread(target=scheduler)
+    t1.start()
