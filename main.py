4ca2d9cd999cf61e68e3148b1e35350e1c0931bb
TheJokingJack <89432992+TheJokingJack@users.noreply.github.com>
2021-09-03 21:36:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ bot = commands.Bot(command_prefix='$')
 
 @commands.command(aliases=['ttt'])
 async def test(ctx, *args):
-  await ctx.send(str(args))
+    await ctx.send(str(args))
 
 @commands.command()
 async def james(ctx):
-  await ctx.send('Isaac is ricj')
+    await ctx.send('Isaac is ricj')
 
 @commands.command()
 async def isaac(ctx):
-  await ctx.send('James is ricj')
+    await ctx.send('James is ricj')
 
 bot.add_command(test)
 bot.add_command(james)
