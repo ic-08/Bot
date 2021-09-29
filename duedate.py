3b10b30bc53551e7ffeef062ed2b944218da897b
TheJokingJack <89432992+TheJokingJack@users.noreply.github.com>
2021-09-29 10:02:59 -0400

    Create duedate.py

---
+++ duedate.py
@@ -0,0 +1,33 @@
+import discord
+def cng_date():
+    embed = discord.Embed(
+        title = "Due Dates", 
+        color = 0x808080)
+    #805 Homework
+    embed.add_field(name = "805", value = "MATH 3.3 # 1-6\n3.5 #1-6\n3.9 #7-12\nDue Sept 29th\n\nLook Both Ways Reading Reflections\nDue Oct 18\n\nDaily Agenda September\nDue Oct 1st\n#1 Mental Health\nFirst 5 slides Due Sept 28th\n*FOR WORKAHOLICS*\nScratch 2 - Pet the Cat\nNo due date\nScratch 3 - Animate\nNo Due Date\nScratch 4 - Pop a Balloon\nNo Due Date\nScratch 5 - Play the Drums\nNo Due Date\nScratch Choice Challenges\nNo due date\nSelf Portrait Art\nI don't know the due date but its for homework if unfinished\n",  inline = False)
+    
+    #705 Homework 
+    embed.add_field(name = "705", value = "Bienvenue au camp: Lecture et compréhension (IDK MAN)\n\nSignpost #3 - Tough decisions\n- Finish it by Sept. 28th\nArt - Mr. Cobban is our teacher now. We will be coninuing with Pop Art assignment with him.\n\n Remember to email Mr. Cobban so he can look at how you write emails!\n\n Tree Diagram Observations have to finished. \n\n Scratch 1 was due last Monday ( FINISH YOUR WORK OR MR. WILL WON'T BE HAPPY. )", inline = False)
+    
+    #General ( Applies to all )
+    embed.add_field(name = "General", value = "**HAVE YOUR PARENTS FILL OUT YOUR COVID SCREENING SHEET EVERY MORNING**", inline = False)
+    
+    embed.set_footer(text="Written with Python. Updated 2021-09-28 3:29 p.m.") #lmao ops
+    return embed
+    
+    #how do i save it
+    #Its better to use dates instead of today or tmr because you wont have to update it tomorrow if you forget.
+    #And then update the time here                   Here look up
+    
+    #INSTRUCTIONS -----> say daniel is ricj before anything else 👍
+    #ANY QUESTIONS FEEL FREE TO ASK ME JAMES OR ANY DEV DANIEL IS THE SMARTEST ONE THO    
+
+    #Ping any admins or devs if you think something is wrong or not up to date, or if there is an error with any of the stuff in main.py. Refrain from editing the code there even if you know what ur doing
+    
+    #If you find that the bot is NOT running, please let me know or any of the admins ASAP
+    
+    #FOr reference, try not to use ( "Work" should be due tmr, or seomthing like that because then you will have to change it every day)
+
+    #daniel is afk
+    #derek is afk
+    #isaac is afk
